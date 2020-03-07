3113150af280b1dbfc0df666362cede75be3769a
shaifaligupta <shaifaligupta@192.168.1.59>
2020-03-07 09:02:52 +0530

    test

---
+++ src/main/java/com/example/demo/Java8Demo/StreamIOSamples.java
@@ -0,0 +1,20 @@
+package com.example.demo.Java8Demo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StreamIOSamples {
+	
+	@RequestMapping("/getList")
+	public List<String> getListFromSample(){
+		List<String> list1 = Arrays.asList("One","two","three","four");
+		List<String> filteredList =list1.stream().filter(t -> t.startsWith("t")).collect(Collectors.toList());
+		return filteredList;
+	}
+
+}
